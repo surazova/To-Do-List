46f31805cf4343017c96c3a89ad2d9f5eab42331
Sitora Urazova <sitoraurazova@gmail.com>
2019-02-06 19:24:14 +0000

    Edited js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Selecting the elements 
+var enterButton = document.getElementbyId("enter");
+
+// Functions for events
+
+
+// Event Listeners
+enterButton.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+
+
+// 1. get the element (getElementById)
+// 2. Add the event listener (addEventListener)
+// 3. add function for that event (function addListAfterClick)
